ad9af5d185059011d0eac737d1a3750d2557388f
Eik Emil Christensen <me@eikc.dk>
2018-07-21 19:16:13 +0200

    Added the final flow of the application logic including slack notifications

--- dinero.go
+++ dinero.go
@@ -1,1 +1,112 @@
 package main
+
+import (
+	"github.com/eikc/dinero-go"
+	"github.com/eikc/dinero-go/contacts"
+	"github.com/eikc/dinero-go/invoices"
+)
+
+type dineroAPI struct {
+	dinero.API
+}
+
+type invoiceCreated struct {
+	ID        string
+	Number    int
+	Timestamp string
+}
+
+func (api *dineroAPI) CreateCustomer(email, name, address string) (string, error) {
+	cParams := contacts.ContactParams{
+		IsPerson:             true,
+		Name:                 name,
+		Email:                email,
+		Street:               address,
+		CountryKey:           "DK",
+		PaymentConditionType: dinero.NettoCash,
+	}
+
+	c, err := contacts.Add(api, cParams)
+	if err != nil {
+		return "", err
+	}
+
+	return c.ID, nil
+}
+
+func (api *dineroAPI) CreateInvoice(customerID, description string, amount int64) (*invoiceCreated, error) {
+	invoiceParams := invoices.CreateInvoice{
+		ContactID:        customerID,
+		ShowLinesInclVat: true,
+		Currency:         "DKK",
+		Language:         "da-DK",
+		Date:             dinero.DateNow(),
+		ProductLines: []invoices.InvoiceLine{
+			invoices.InvoiceLine{
+				BaseAmountValue: float64((amount / 100)),
+				Quantity:        1,
+				AccountNumber:   1000,
+				Description:     description,
+				LineType:        "Product",
+				Unit:            "parts",
+			},
+		},
+	}
+
+	timestamp, err := invoices.Save(api, invoiceParams)
+	if err != nil {
+		return nil, err
+	}
+
+	invoice, err := invoices.Get(api, timestamp.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &invoiceCreated{invoice.ID, invoice.Number, invoice.Timestamp}, nil
+}
+
+func (api *dineroAPI) BookInvoice(invoiceID, timestamp string) (*invoiceCreated, error) {
+	_, err := invoices.Book(api, invoiceID, timestamp)
+	if err != nil {
+		return nil, err
+	}
+
+	invoice, err := invoices.Get(api, invoiceID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &invoiceCreated{invoice.ID, invoice.Number, invoice.Timestamp}, nil
+}
+
+func (api *dineroAPI) CreatePayment(invoiceID string, amount int64) error {
+	invoice, err := invoices.Get(api, invoiceID)
+	if err != nil {
+		return err
+	}
+
+	payment := invoices.CreatePaymentParams{
+		Amount:               float64(amount / 100),
+		DepositAccountNumber: 55000,
+		Description:          "Paid with stripe",
+		PaymentDate:          dinero.DateNow(),
+		Timestamp:            invoice.Timestamp,
+	}
+
+	_, err = invoices.CreatePayment(api, invoiceID, payment)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (api *dineroAPI) SendInvoice(invoiceID string) error {
+	_, err := invoices.SendEmail(api, invoiceID, invoices.SendInvoice{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"strconv"
-
-	"github.com/eikc/dinero-go/contacts"
-	"github.com/eikc/dinero-go/invoices"
 
 	"github.com/eikc/dinero-go/dinerotest"
 
@@ -17,6 +15,15 @@ import (
 	"github.com/julienschmidt/httprouter"
 	stripe "github.com/stripe/stripe-go"
 	stripeOrders "github.com/stripe/stripe-go/order"
+)
+
+const (
+	flowStatus            = "flowstatus"
+	customerCreatedStatus = "UserCreated"
+	invoiceCreatedStatus  = "InvoiceCreated"
+	invoiceBookedStatus   = "invoiceBooked"
+	invoicePaidStatus     = "InvoicePaid"
+	invoiceSentStatus     = "EmailSent"
 )
 
 type order struct {
@@ -32,12 +39,15 @@ func main() {
 	stripe.Key = "sk_test_XWq2CSR4oPhh80dX1QCBfs6y"
 	client, secret, apiKey, orgID := dinerotest.GetClientKeysForIntegrationTesting()
 	dineroClient := dinero.NewClient(client, secret)
-	dineroClient.Authorize(apiKey, orgID)
+
+	api := dineroAPI{
+		API: dineroClient,
+	}
 
 	router := httprouter.New()
 	router.GET("/", index)
 	router.POST("/create", create())
-	router.POST("/webhook", webhookReceiver(dineroClient))
+	router.POST("/webhook", webhookReceiver(&api, apiKey, orgID))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -46,64 +56,21 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
-func webhookReceiver(api dinero.API) httprouter.Handle {
+func webhookReceiver(api *dineroAPI, apiKey string, orgID int) httprouter.Handle {
 
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		api.Authorize(apiKey, orgID)
 		decoder := json.NewDecoder(r.Body)
 
 		var e stripe.Event
 		decoder.Decode(&e)
-
-		fmt.Println("event is: ", e)
 
 		switch e.Type {
 		case "order.created":
 			var o stripe.Order
 			err := json.Unmarshal(e.Data.Raw, &o)
 			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			fmt.Println("creating dinero invoice..")
-			cParams := contacts.ContactParams{
-				IsPerson:             true,
-				Name:                 o.Metadata["name"],
-				Email:                o.Email,
-				Street:               o.Metadata["address"],
-				CountryKey:           "DK",
-				PaymentConditionType: dinero.NettoCash,
-			}
-
-			c, err := contacts.Add(api, cParams)
-			if err != nil {
-				fmt.Println("what should we do? err: ", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-			invoiceParams := invoices.CreateInvoice{
-				ContactID:        c.ID,
-				ShowLinesInclVat: true,
-				Currency:         "DKK",
-				Language:         "da-DK",
-				Description:      "Awesome badass program",
-				Date:             dinero.DateNow(),
-				ProductLines: []invoices.InvoiceLine{
-					invoices.InvoiceLine{
-						BaseAmountValue: float64((o.Amount / 100)),
-						Quantity:        1,
-						AccountNumber:   1000,
-						Description:     "Fitness program",
-						LineType:        "Product",
-						Unit:            "parts",
-					},
-				},
-			}
-
-			invoice, err := invoices.Save(api, invoiceParams)
-			if err != nil {
-				fmt.Println("what should we do? err: ", err)
+				go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
@@ -112,57 +79,154 @@ func webhookReceiver(api dinero.API) httprouter.Handle {
 			token := o.Metadata["token"]
 			op := &stripe.OrderPayParams{}
 			op.SetSource(token) // obtained with Stripe.js
-			op.AddMetadata("invoiceID", invoice.ID)
-			op.AddMetadata("invoiceTimestamp", invoice.Timestamp)
 			stripeOrders.Pay(o.ID, op)
-			fmt.Println("order paid, invoice created")
+
+			// go slackLogging(fmt.Sprintf("Order %v", o.ID), "Stripe charged succesfully", "Paid in stripe", "#2eb886")
 
 		case "order.payment_succeeded":
 			var o stripe.Order
 			err := json.Unmarshal(e.Data.Raw, &o)
 			if err != nil {
+				go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
 
-			invoiceID := o.Metadata["invoiceID"]
-			invoiceTimestamp := o.Metadata["invoiceTimestamp"]
-
-			timestamp, err := invoices.Book(api, invoiceID, invoiceTimestamp)
+			name := o.Metadata["name"]
+			email := o.Metadata["email"]
+			address := o.Metadata["address"]
+			contactID, err := api.CreateCustomer(email, name, address)
 			if err != nil {
+				go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Println("booking invoice failed: ", err)
 				return
 			}
 
-			payment := invoices.CreatePaymentParams{
-				Amount:               float64(o.Amount / 100),
-				DepositAccountNumber: 55000,
-				Description:          "Paid with stripe",
-				PaymentDate:          dinero.DateNow(),
-				Timestamp:            timestamp.Timestamp,
-			}
-
-			timestamp, err = invoices.CreatePayment(api, invoiceID, payment)
+			updatedOrder := stripe.OrderUpdateParams{}
+			updatedOrder.AddMetadata(flowStatus, customerCreatedStatus)
+			updatedOrder.AddMetadata("customer", contactID)
+			_, err = stripeOrders.Update(o.ID, &updatedOrder)
 			if err != nil {
+				go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
 				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Println("paying invoice failed: ", err)
 				return
 			}
 
-			fmt.Println("sending invoice...")
-			_, err = invoices.SendEmail(api, invoiceID, invoices.SendInvoice{})
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				fmt.Println("sending invoice failed ", err)
-				return
-			}
+			// go slackLogging("Order "+o.ID, "Contact created in dinero", customerCreatedStatus, "#2eb886")
 
-			fmt.Println("yes order paid, sending pdf")
 		case "order.payment_failed":
 			fmt.Println("order payment failed, what to do!?!? :(")
 		case "charge.refunded":
 			fmt.Println("too bad :-(")
+
+		case "order.updated":
+			var o stripe.Order
+			err := json.Unmarshal(e.Data.Raw, &o)
+			if err != nil {
+				go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			flow := o.Metadata[flowStatus]
+
+			switch flow {
+			case customerCreatedStatus:
+				customerID := o.Metadata["customer"]
+				amount := o.Amount
+				productName := o.Items[0].Description
+				invoice, err := api.CreateInvoice(customerID, productName, amount)
+				if err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				updatedOrder := stripe.OrderUpdateParams{}
+				updatedOrder.AddMetadata(flowStatus, invoiceCreatedStatus)
+				updatedOrder.AddMetadata("invoiceID", invoice.ID)
+				updatedOrder.AddMetadata("invoiceTimestamp", invoice.Timestamp)
+
+				_, err = stripeOrders.Update(o.ID, &updatedOrder)
+				if err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				// go slackLogging("Order "+o.ID, "Invoice created in draft mode", invoiceCreatedStatus, "#2eb886")
+
+			case invoiceCreatedStatus:
+				invoiceID := o.Metadata["invoiceID"]
+				timestamp := o.Metadata["invoiceTimestamp"]
+
+				invoice, err := api.BookInvoice(invoiceID, timestamp)
+				if err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					fmt.Println("error occured: ", err)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				updatedOrder := stripe.OrderUpdateParams{}
+				updatedOrder.AddMetadata(flowStatus, invoiceBookedStatus)
+				updatedOrder.AddMetadata("invoiceTimestamp", invoice.Timestamp)
+				updatedOrder.AddMetadata("invoiceNumber", strconv.FormatInt(int64(invoice.Number), 10))
+
+				_, err = stripeOrders.Update(o.ID, &updatedOrder)
+				if err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				// go slackLogging("Order "+o.ID, fmt.Sprint("invoice booked: ", invoice.Number), invoiceBookedStatus, "#2eb886")
+
+			case invoiceBookedStatus:
+				invoiceID := o.Metadata["invoiceID"]
+
+				if err := api.CreatePayment(invoiceID, o.Amount); err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				updatedOrder := stripe.OrderUpdateParams{}
+				updatedOrder.AddMetadata(flowStatus, invoicePaidStatus)
+
+				_, err = stripeOrders.Update(o.ID, &updatedOrder)
+				if err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				// go slackLogging("Order "+o.ID, "Payment created for invoice", invoicePaidStatus, "#2eb886")
+
+			case invoicePaidStatus:
+				invoiceID := o.Metadata["invoiceID"]
+				if err := api.SendInvoice(invoiceID); err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				updatedOrder := stripe.OrderUpdateParams{}
+				updatedOrder.AddMetadata(flowStatus, invoiceSentStatus)
+				updatedOrder.Status = stripe.String(string(stripe.OrderStatusFulfilled))
+
+				_, err = stripeOrders.Update(o.ID, &updatedOrder)
+				if err != nil {
+					go slackLogging(fmt.Sprintf("Order %v", o.ID), err.Error(), "Error with order", "#CF0003")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				// go slackLogging("Order "+o.ID, "Invoice sent to customer", invoiceSentStatus, "#2eb886")
+
+			case invoiceSentStatus:
+				go slackLogging("Order "+o.ID, fmt.Sprintf(":gopher_dance: Well done, you just earned: %v DKK :gopher_dance:", o.Amount/100), "Completed", "#23D1E1")
+			}
 		}
 
 		w.WriteHeader(http.StatusOK)
@@ -188,12 +252,14 @@ func create() httprouter.Handle {
 				},
 			},
 		}
+
 		mashalledOrder, _ := json.Marshal(o)
 		params.AddMetadata("name", o.Name)
 		params.AddMetadata("newsletter", strconv.FormatBool(o.Newsletter))
 		params.AddMetadata("rawData", string(mashalledOrder))
 		params.AddMetadata("address", o.Address)
 		params.AddMetadata("token", o.StripeToken)
+		params.AddMetadata("email", o.Email)
 
 		_, err = stripeOrders.New(params)
 		if err != nil {
@@ -201,6 +267,32 @@ func create() httprouter.Handle {
 			return
 		}
 
+		slackLogging("New order received!", "YES! You are BADASS!! :tada: :the_horns: :rocket:", "new", "#2eb886")
 		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func slackLogging(title, text, status, color string) {
+	url := "https://hooks.slack.com/services/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"
+	attachment1 := Attachment{}
+	attachment1.addField(Field{Title: "Title", Value: title})
+	attachment1.addField(Field{Title: "Status", Value: status})
+	attachment1.addField(Field{Title: "Extra info", Value: text})
+	attachment1.AuthorIcon = stripe.String(":gopher_dance:")
+	attachment1.Color = stripe.String(color)
+
+	payload := Payload{
+		Username:    "robot",
+		Channel:     "#logging",
+		IconEmoji:   ":gopher_dance:",
+		Attachments: []Attachment{attachment1},
+	}
+
+	json, _ := json.Marshal(payload)
+	reader := bytes.NewReader(json)
+
+	_, err := http.Post(url, "application/json", reader)
+	if err != nil {
+		fmt.Println("slack error occured: ", err)
 	}
 }

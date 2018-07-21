d5896fb868958d4d3f89c424c23207083f168cc1
Eik Emil Christensen <me@eikc.dk>
2018-07-21 12:31:57 +0200

    added first implementation of the dinero integration

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/eikc/dinero-go/contacts"
+	"github.com/eikc/dinero-go/invoices"
+
+	"github.com/eikc/dinero-go/dinerotest"
+
+	"github.com/eikc/dinero-go"
+
 	"github.com/julienschmidt/httprouter"
 	stripe "github.com/stripe/stripe-go"
 	stripeOrders "github.com/stripe/stripe-go/order"
@@ -23,11 +30,14 @@ type order struct {
 
 func main() {
 	stripe.Key = "sk_test_XWq2CSR4oPhh80dX1QCBfs6y"
+	client, secret, apiKey, orgID := dinerotest.GetClientKeysForIntegrationTesting()
+	dineroClient := dinero.NewClient(client, secret)
+	dineroClient.Authorize(apiKey, orgID)
 
 	router := httprouter.New()
 	router.GET("/", index)
 	router.POST("/create", create())
-	router.POST("/webhook", webhookReceiver)
+	router.POST("/webhook", webhookReceiver(dineroClient))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -36,40 +46,127 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
 
-func webhookReceiver(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	decoder := json.NewDecoder(r.Body)
+func webhookReceiver(api dinero.API) httprouter.Handle {
 
-	var e stripe.Event
-	decoder.Decode(&e)
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		decoder := json.NewDecoder(r.Body)
 
-	fmt.Println("event is: ", e)
+		var e stripe.Event
+		decoder.Decode(&e)
 
-	switch e.Type {
-	case "order.created":
-		var o stripe.Order
-		err := json.Unmarshal(e.Data.Raw, &o)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
+		fmt.Println("event is: ", e)
+
+		switch e.Type {
+		case "order.created":
+			var o stripe.Order
+			err := json.Unmarshal(e.Data.Raw, &o)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Println("creating dinero invoice..")
+			cParams := contacts.ContactParams{
+				IsPerson:             true,
+				Name:                 o.Metadata["name"],
+				Email:                o.Email,
+				Street:               o.Metadata["address"],
+				CountryKey:           "DK",
+				PaymentConditionType: dinero.NettoCash,
+			}
+
+			c, err := contacts.Add(api, cParams)
+			if err != nil {
+				fmt.Println("what should we do? err: ", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			invoiceParams := invoices.CreateInvoice{
+				ContactID:        c.ID,
+				ShowLinesInclVat: true,
+				Currency:         "DKK",
+				Language:         "da-DK",
+				Description:      "Awesome badass program",
+				Date:             dinero.DateNow(),
+				ProductLines: []invoices.InvoiceLine{
+					invoices.InvoiceLine{
+						BaseAmountValue: float64((o.Amount / 100)),
+						Quantity:        1,
+						AccountNumber:   1000,
+						Description:     "Fitness program",
+						LineType:        "Product",
+						Unit:            "parts",
+					},
+				},
+			}
+
+			invoice, err := invoices.Save(api, invoiceParams)
+			if err != nil {
+				fmt.Println("what should we do? err: ", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			fmt.Println("yes an order created, paying the order")
+			token := o.Metadata["token"]
+			op := &stripe.OrderPayParams{}
+			op.SetSource(token) // obtained with Stripe.js
+			op.AddMetadata("invoiceID", invoice.ID)
+			op.AddMetadata("invoiceTimestamp", invoice.Timestamp)
+			stripeOrders.Pay(o.ID, op)
+			fmt.Println("order paid, invoice created")
+
+		case "order.payment_succeeded":
+			var o stripe.Order
+			err := json.Unmarshal(e.Data.Raw, &o)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			invoiceID := o.Metadata["invoiceID"]
+			invoiceTimestamp := o.Metadata["invoiceTimestamp"]
+
+			timestamp, err := invoices.Book(api, invoiceID, invoiceTimestamp)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Println("booking invoice failed: ", err)
+				return
+			}
+
+			payment := invoices.CreatePaymentParams{
+				Amount:               float64(o.Amount / 100),
+				DepositAccountNumber: 55000,
+				Description:          "Paid with stripe",
+				PaymentDate:          dinero.DateNow(),
+				Timestamp:            timestamp.Timestamp,
+			}
+
+			timestamp, err = invoices.CreatePayment(api, invoiceID, payment)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Println("paying invoice failed: ", err)
+				return
+			}
+
+			fmt.Println("sending invoice...")
+			_, err = invoices.SendEmail(api, invoiceID, invoices.SendInvoice{})
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				fmt.Println("sending invoice failed ", err)
+				return
+			}
+
+			fmt.Println("yes order paid, sending pdf")
+		case "order.payment_failed":
+			fmt.Println("order payment failed, what to do!?!? :(")
+		case "charge.refunded":
+			fmt.Println("too bad :-(")
 		}
 
-		token := o.Metadata["token"]
-
-		fmt.Println("yes an order created, paying the order")
-		op := &stripe.OrderPayParams{}
-		op.SetSource(token) // obtained with Stripe.js
-		stripeOrders.Pay(o.ID, op)
-		fmt.Println("order paid")
-
-	case "order.payment_succeeded":
-		fmt.Println("yes order paid, sending pdf")
-	case "order.payment_failed":
-		fmt.Println("order payment failed, what to do!?!? :(")
-	case "charge.refunded":
-		fmt.Println("too bad :-(")
+		w.WriteHeader(http.StatusOK)
 	}
-
-	w.WriteHeader(http.StatusOK)
 }
 
 func create() httprouter.Handle {
@@ -92,6 +189,7 @@ func create() httprouter.Handle {
 			},
 		}
 		mashalledOrder, _ := json.Marshal(o)
+		params.AddMetadata("name", o.Name)
 		params.AddMetadata("newsletter", strconv.FormatBool(o.Newsletter))
 		params.AddMetadata("rawData", string(mashalledOrder))
 		params.AddMetadata("address", o.Address)

2fb7acc254ae230287577ae82b274eaa296c75bb
Eik Emil Christensen <me@eikc.dk>
2018-08-14 23:09:51 +0200

    refactored webhook into a program workflow in its own file

---
+++ program_workflow.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/stripe/stripe-go"
+	stripeClient "github.com/stripe/stripe-go/client"
+)
+
+// ProgramWorkFlow handles all actions surrounding the workflow of a program
+type ProgramWorkFlow struct {
+	DineroAPI  *dineroAPI
+	StripeAPI  *stripeClient.API
+	httpClient *http.Client
+}
+
+// StartFlow Controls the workflow of the order
+func (workflow *ProgramWorkFlow) StartFlow(o stripe.Order) (string, error) {
+	flow := o.Metadata[flowStatus]
+	var err error
+
+	switch flow {
+	case customerCreatedStatus:
+		err = workflow.CreateInvoice(o)
+	case invoiceCreatedStatus:
+		err = workflow.BookInvoice(o)
+	case invoiceBookedStatus:
+		err = workflow.CreatePayment(o)
+	case invoicePaidStatus:
+		err = workflow.FulfillWorkflow(o)
+	case invoiceSentStatus:
+		workflow.CelebrateOrder(o)
+	}
+
+	return flow, err
+}
+
+// CreateCustomer Creates a customer in the given workflow
+func (workflow *ProgramWorkFlow) CreateCustomer(o stripe.Order) error {
+	name := o.Metadata["name"]
+	email := o.Metadata["email"]
+	address := o.Metadata["address"]
+	contactID, err := workflow.DineroAPI.CreateCustomer(email, name, address)
+	if err != nil {
+		return fmt.Errorf("Error creating contact in Dinero: %v", err.Error())
+	}
+
+	updatedOrder := stripe.OrderUpdateParams{}
+	updatedOrder.AddMetadata(flowStatus, customerCreatedStatus)
+	updatedOrder.AddMetadata("customer", contactID)
+	_, err = workflow.StripeAPI.Orders.Update(o.ID, &updatedOrder)
+	if err != nil {
+		return fmt.Errorf("Error updating stripe order: %v", err.Error())
+	}
+
+	return nil
+}
+
+// CreateInvoice creates an invoice in the given workflow
+func (workflow *ProgramWorkFlow) CreateInvoice(o stripe.Order) error {
+	customerID := o.Metadata["customer"]
+	amount := o.Amount
+	productName := o.Items[0].Description
+	invoice, err := workflow.DineroAPI.CreateInvoice(customerID, productName, amount)
+	if err != nil {
+		return fmt.Errorf("Dinero API - Creating invoice: %s", err.Error())
+	}
+
+	updatedOrder := stripe.OrderUpdateParams{}
+	updatedOrder.AddMetadata(flowStatus, invoiceCreatedStatus)
+	updatedOrder.AddMetadata("invoiceID", invoice.ID)
+	updatedOrder.AddMetadata("invoiceTimestamp", invoice.Timestamp)
+
+	_, err = workflow.StripeAPI.Orders.Update(o.ID, &updatedOrder)
+	if err != nil {
+		return fmt.Errorf("Stripe API - Error updating state to %v", invoiceCreatedStatus)
+	}
+
+	return nil
+}
+
+// BookInvoice knows the workflow surrounding booking an invoice in dinero and update the order accordingly
+func (workflow *ProgramWorkFlow) BookInvoice(o stripe.Order) error {
+	invoiceID := o.Metadata["invoiceID"]
+	timestamp := o.Metadata["invoiceTimestamp"]
+
+	invoice, err := workflow.DineroAPI.BookInvoice(invoiceID, timestamp)
+	if err != nil {
+		return fmt.Errorf("Dinero API - Error booking invoice: %v", err.Error())
+	}
+
+	updatedOrder := stripe.OrderUpdateParams{}
+	updatedOrder.AddMetadata(flowStatus, invoiceBookedStatus)
+	updatedOrder.AddMetadata("invoiceTimestamp", invoice.Timestamp)
+	updatedOrder.AddMetadata("invoiceNumber", strconv.FormatInt(int64(invoice.Number), 10))
+
+	_, err = workflow.StripeAPI.Orders.Update(o.ID, &updatedOrder)
+	if err != nil {
+		return fmt.Errorf("Stripe API - Error updating stripe order: %v", err.Error())
+	}
+
+	return nil
+}
+
+// CreatePayment is the workflow that creates an payment in dinero and update the stripe order to the current state
+func (workflow *ProgramWorkFlow) CreatePayment(o stripe.Order) error {
+	invoiceID := o.Metadata["invoiceID"]
+
+	if err := workflow.DineroAPI.CreatePayment(invoiceID, o.Amount); err != nil {
+		return fmt.Errorf("Dinero API - error creating payment: %s", err.Error())
+	}
+
+	updatedOrder := stripe.OrderUpdateParams{}
+	updatedOrder.AddMetadata(flowStatus, invoicePaidStatus)
+
+	_, err := workflow.StripeAPI.Orders.Update(o.ID, &updatedOrder)
+	if err != nil {
+		return fmt.Errorf("Stripe Api - Error updating state to %v", invoicePaidStatus)
+	}
+
+	return nil
+}
+
+// FulfillWorkflow finalizes the workflow
+func (workflow *ProgramWorkFlow) FulfillWorkflow(o stripe.Order) error {
+	order, err := workflow.StripeAPI.Orders.Get(o.ID, nil)
+	if err != nil {
+		return fmt.Errorf("Stripe API - Error getting stripe order: %s", err.Error())
+	}
+
+	if order.Status == string(stripe.OrderStatusFulfilled) {
+		return nil
+	}
+
+	invoiceID := o.Metadata["invoiceID"]
+	if err := workflow.DineroAPI.SendInvoice(invoiceID); err != nil {
+		return fmt.Errorf("Dinero API - Error sending email: %s", err.Error())
+	}
+
+	updatedOrder := stripe.OrderUpdateParams{}
+	updatedOrder.AddMetadata(flowStatus, invoiceSentStatus)
+	updatedOrder.Status = stripe.String(string(stripe.OrderStatusFulfilled))
+
+	_, err = workflow.StripeAPI.Orders.Update(o.ID, &updatedOrder)
+	if err != nil {
+		return fmt.Errorf("Stripe API - Error updating state to %v", invoiceSentStatus)
+	}
+
+	return nil
+}
+
+// CelebrateOrder knows how to fucking celebrate mate!
+func (workflow *ProgramWorkFlow) CelebrateOrder(o stripe.Order) {
+	name := o.Metadata["name"]
+	slackLogging(workflow.httpClient,
+		"Order "+o.ID,
+		fmt.Sprintf(":gopher_dance: Well done, you just earned: %v DKK and %s will be a badass :gopher_dance:", o.Amount/100, name),
+		"Completed",
+		"#23D1E1")
+}
--- webhook.go
+++ webhook.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 	stripe "github.com/stripe/stripe-go"
@@ -19,11 +18,14 @@ func webhookReceiver() httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		c := appengine.NewContext(r)
 		s := getSettings(c)
-
 		httpClient := urlfetch.Client(c)
-
 		api := getClient(c)
 		stripeAPI := getStripe(c)
+		programWorkflow := ProgramWorkFlow{
+			DineroAPI:  api,
+			StripeAPI:  stripeAPI,
+			httpClient: httpClient,
+		}
 
 		var e stripe.Event
 		if appengine.IsDevAppServer() {
@@ -107,25 +109,12 @@ func webhookReceiver() httprouter.Handle {
 				return
 			}
 
-			name := o.Metadata["name"]
-			email := o.Metadata["email"]
-			address := o.Metadata["address"]
-			contactID, err := api.CreateCustomer(email, name, address)
-			if err != nil {
-				errorHandling(w, err)
-				go slackLogging(httpClient, fmt.Sprintf("Order %v", o.ID), err.Error(), "Error creating contact in Dinero", "#CF0003")
-				return
-			}
-
-			updatedOrder := stripe.OrderUpdateParams{}
-			updatedOrder.AddMetadata(flowStatus, customerCreatedStatus)
-			updatedOrder.AddMetadata("customer", contactID)
-			_, err = stripeAPI.Orders.Update(o.ID, &updatedOrder)
+			err = programWorkflow.CreateCustomer(o)
 			if err != nil {
 				errorHandling(w, err)
 				slackLogging(httpClient,
 					fmt.Sprintf("Order %v", o.ID), err.Error(),
-					fmt.Sprintf("Stripe API - Error updating state to %v", customerCreatedStatus),
+					fmt.Sprintf("Workflow: %v", customerCreatedStatus),
 					"#CF0003")
 				return
 			}
@@ -144,123 +133,14 @@ func webhookReceiver() httprouter.Handle {
 				return
 			}
 
-			flow := o.Metadata[flowStatus]
-
-			switch flow {
-			case customerCreatedStatus:
-				customerID := o.Metadata["customer"]
-				amount := o.Amount
-				productName := o.Items[0].Description
-				invoice, err := api.CreateInvoice(customerID, productName, amount)
-				if err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient, fmt.Sprintf("Order %v", o.ID), err.Error(), "Error talking to Dinero - Creating invoice", "#CF0003")
-					return
-				}
-
-				updatedOrder := stripe.OrderUpdateParams{}
-				updatedOrder.AddMetadata(flowStatus, invoiceCreatedStatus)
-				updatedOrder.AddMetadata("invoiceID", invoice.ID)
-				updatedOrder.AddMetadata("invoiceTimestamp", invoice.Timestamp)
-
-				_, err = stripeAPI.Orders.Update(o.ID, &updatedOrder)
-				if err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient,
-						fmt.Sprintf("Order %v", o.ID), err.Error(),
-						fmt.Sprintf("Stripe API - Error updating state to %v", invoiceCreatedStatus),
-						"#CF0003")
-					return
-				}
-
-			case invoiceCreatedStatus:
-				invoiceID := o.Metadata["invoiceID"]
-				timestamp := o.Metadata["invoiceTimestamp"]
-
-				invoice, err := api.BookInvoice(invoiceID, timestamp)
-				if err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient, fmt.Sprintf("Order %v", o.ID), err.Error(), "Dinero api - Error booking invoice", "#CF0003")
-					return
-				}
-
-				updatedOrder := stripe.OrderUpdateParams{}
-				updatedOrder.AddMetadata(flowStatus, invoiceBookedStatus)
-				updatedOrder.AddMetadata("invoiceTimestamp", invoice.Timestamp)
-				updatedOrder.AddMetadata("invoiceNumber", strconv.FormatInt(int64(invoice.Number), 10))
-
-				_, err = stripeAPI.Orders.Update(o.ID, &updatedOrder)
-				if err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient,
-						fmt.Sprintf("Order %v", o.ID), err.Error(),
-						fmt.Sprintf("Stripe API - Error updating state to %v", invoiceBookedStatus),
-						"#CF0003")
-					return
-				}
-
-			case invoiceBookedStatus:
-				invoiceID := o.Metadata["invoiceID"]
-
-				if err := api.CreatePayment(invoiceID, o.Amount); err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient, fmt.Sprintf("Order %v", o.ID), err.Error(), "Dinero API - Error creating payment", "#CF0003")
-					return
-				}
-
-				updatedOrder := stripe.OrderUpdateParams{}
-				updatedOrder.AddMetadata(flowStatus, invoicePaidStatus)
-
-				_, err = stripeAPI.Orders.Update(o.ID, &updatedOrder)
-				if err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient,
-						fmt.Sprintf("Order %v", o.ID), err.Error(),
-						fmt.Sprintf("Stripe API - Error updating state to %v", invoicePaidStatus),
-						"#CF0003")
-					return
-				}
-
-			case invoicePaidStatus:
-				order, err := stripeAPI.Orders.Get(o.ID, nil)
-				if err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient, fmt.Sprintf("Order %v", o.ID), err.Error(), "Stripe API - error getting stripe order", "#CF0003")
-					return
-				}
-
-				if order.Status == string(stripe.OrderStatusFulfilled) {
-					break
-				}
-
-				invoiceID := o.Metadata["invoiceID"]
-				if err := api.SendInvoice(invoiceID); err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient, fmt.Sprintf("Order %v", o.ID), err.Error(), "Dinero API - Error sending email", "#CF0003")
-					return
-				}
-
-				updatedOrder := stripe.OrderUpdateParams{}
-				updatedOrder.AddMetadata(flowStatus, invoiceSentStatus)
-				updatedOrder.Status = stripe.String(string(stripe.OrderStatusFulfilled))
-
-				_, err = stripeAPI.Orders.Update(o.ID, &updatedOrder)
-				if err != nil {
-					errorHandling(w, err)
-					slackLogging(httpClient,
-						fmt.Sprintf("Order %v", o.ID), err.Error(),
-						fmt.Sprintf("Stripe API - Error updating state to %v", invoiceSentStatus),
-						"#CF0003")
-					return
-				}
-
-			case invoiceSentStatus:
-				name := o.Metadata["name"]
+			workflow, err := programWorkflow.StartFlow(o)
+			if err != nil {
+				errorHandling(w, err)
 				slackLogging(httpClient,
-					"Order "+o.ID,
-					fmt.Sprintf(":gopher_dance: Well done, you just earned: %v DKK and %s will be a badass :gopher_dance:", o.Amount/100, name),
-					"Completed",
-					"#23D1E1")
+					fmt.Sprintf("Order %v", o.ID), err.Error(),
+					fmt.Sprintf("workflow: %v", workflow),
+					"#CF0003")
+				return
 			}
 		}
 

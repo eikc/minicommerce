3fd1e9f21266f695cad7f094030cb322076d74c2
Eik Emil Christensen <me@eikc.dk>
2019-07-28 10:39:00 +0200

    added UUID generator pkg for abstracting away the pkg for easier testability

---
+++ pkg/uuid/generator.go
@@ -0,0 +1,20 @@
+package uuid
+
+import (
+	"github.com/gofrs/uuid"
+)
+
+// Generator struct knows how to generate ID's in minicommerce
+// it is the recommened way to generate ID's
+type Generator struct {
+}
+
+// NewGenerator is the constructor function for a Generator
+func NewGenerator() *Generator {
+	return &Generator{}
+}
+
+// New generates a new UUID v4 ID to use for models
+func (g *Generator) New() (uuid.UUID, error) {
+	return uuid.NewV4()
+}
